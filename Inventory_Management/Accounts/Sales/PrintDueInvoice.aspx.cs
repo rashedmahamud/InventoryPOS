4d5120f06a74f67b524601098cb1f17810974e2d
rashedmahamud <Administrator@RASHED>
2019-01-24 16:36:23 +0600

    Sales___PrintMangeInvoice_report

--- Inventory_Management/Accounts/Sales/PrintDueInvoice.aspx.cs
+++ Inventory_Management/Accounts/Sales/PrintDueInvoice.aspx.cs
@@ -14,18 +14,27 @@ public partial class Accounts_Sales_PrintDueInvoice : System.Web.UI.Page
     string ConnectionString = ConfigurationManager.ConnectionStrings["PointofSaleConstr"].ConnectionString;
     protected void Page_Load(object sender, EventArgs e)
     {
-        if(!IsPostBack){
+        if (!IsPostBack)
+        {
             PrintDueInvoice();
         }
 
     }
 
-    public void PrintDueInvoice() {
+    public void PrintDueInvoice()
+    {
 
-
-
+        string ss = (string)Session["ShopID"];
+        string CompanyName = null;
+        string ComapanyAddress = null;
+        string CompanyMobileNumber = null;
+        string CompanyWebsite = null;
+        string CompanyFooterMassage = null;
+        string CompanyLogo = null;
+        List<DueInvoiceList> dueList = new List<DueInvoiceList>();
         try
         {
+
             SqlConnection cn = new SqlConnection(ConnectionString);
             SqlCommand cmd = new SqlCommand();
             cmd.CommandType = CommandType.Text;
@@ -37,72 +46,87 @@ public partial class Accounts_Sales_PrintDueInvoice : System.Web.UI.Page
 
             SqlDataReader rd = cmd.ExecuteReader();
 
-             var dt = new DataTable();
-             dt.Load(rd);
-             List<DataRow> dr = dt.AsEnumerable().ToList();
-
-                List<DueInvoiceList> dueList = new List<DueInvoiceList>();
-                for (int i = 0; i < dr.Count; i++)
-                {
-                    DueInvoiceList list = new DueInvoiceList();
-
-                    list.InvoiceNo = dr[i].ItemArray[0].ToString();
-                    list.CustomerName = dr[i].ItemArray[1].ToString();
-                    list.Totalpayable = Convert.ToDouble(dr[i].ItemArray[2]);
-                    list.PaidAmount = Convert.ToDouble(dr[i].ItemArray[3]);
-                    list.DueAmount =Convert.ToDouble(dr[i].ItemArray[4]);
-                    list.ShopId = dr[i].ItemArray[5].ToString();
-                    list.Ordedate = dr[i].ItemArray[6].ToString();
-                    dueList.Add(list);
-                }
+            var dt = new DataTable();
+            dt.Load(rd);
+            List<DataRow> dr = dt.AsEnumerable().ToList();
 
 
-            //var reportParameters = new ReportParameterCollection
-            //   {
-            //       new ReportParameter("CompanyName",CompanyName),
-            //       new ReportParameter("ComapanyAddress",ComapanyAddress),
-            //       new ReportParameter("CompanyMobileNumber",CompanyMobileNumber),
-            //       new ReportParameter("CompanyWebsite",CompanyWebsite),
-            //       new ReportParameter("CompanyFooterMassage",CompanyFooterMassage),
+            for (int i = 0; i < dr.Count; i++)
+            {
+                DueInvoiceList list = new DueInvoiceList();
 
-            //       new ReportParameter("CustomerID",CustomerID),
-            //       new ReportParameter("CustomerName",CustomerName),
-            //       new ReportParameter("CustomerMobileNumber",customerMobileNumber),
-            //       new ReportParameter("BillTO",BillTO),
-
-            //       new ReportParameter("BankName",bankName),
-            //       new ReportParameter("AccountName",accountName),
-            //       new ReportParameter("AccountNumber",accountNumber),
-            //       new ReportParameter("SubTotal",SubTotal),
-            //       new ReportParameter ("VAT_Calculation_on_Item", VAT_Calculation_on_Item),
-            //       new ReportParameter("VAT_Percent", VAT_Percent),
-            //       new ReportParameter("Paid",Paid),
-            //       new ReportParameter("Due",Due),
-            //       new ReportParameter("TotalQty",TotalQty),
-            //       new ReportParameter("Total_after_adding_vat",Total_after_adding_vat)
-
-            //   };
-
-
-            ReportViewer1.ProcessingMode = ProcessingMode.Local;
-            ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/RDLCReports/DueInvoiceReport.rdlc");
-
-            ReportDataSource datasource = new ReportDataSource("DueInvoice", dueList);
-            ReportViewer1.LocalReport.DataSources.Clear();
-
-            ReportViewer1.LocalReport.EnableExternalImages = true;
-            ReportViewer1.ExportContentDisposition = ContentDisposition.AlwaysInline;
-
-            ReportViewer1.LocalReport.DataSources.Add(datasource);
-            //ReportViewer1.LocalReport.SetParameters(reportParameters);
+                list.InvoiceNo = dr[i].ItemArray[0].ToString();
+                list.CustomerName = dr[i].ItemArray[1].ToString();
+                list.Totalpayable = Convert.ToDouble(dr[i].ItemArray[2]);
+                list.PaidAmount = Convert.ToDouble(dr[i].ItemArray[3]);
+                list.DueAmount = Convert.ToDouble(dr[i].ItemArray[4]);
+                list.ShopId = dr[i].ItemArray[5].ToString();
+                list.Ordedate = dr[i].ItemArray[6].ToString();
+                dueList.Add(list);
+            }
             cn.Close();
+
         }
-        catch
+        catch { }
+
+
+        // Company Info
+
+        try
         {
-            //lbtotalRow.Text = "No Records Found";
+            SqlConnection cn = new SqlConnection(ConnectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            cn.Open();
+            cmd.CommandText = "Select * from tbl_settings where Location='" + ss + "'";
+            cmd.Connection = cn;
+            SqlDataReader rd4 = cmd.ExecuteReader();
+
+            if (rd4.HasRows)
+            {
+                while (rd4.Read())
+                {
+                    CompanyName = (rd4["CompanyName"].ToString());
+                    ComapanyAddress = rd4["CompanyAddress"].ToString();
+                    CompanyWebsite = rd4["WebAddress"].ToString();
+                    CompanyMobileNumber = rd4["Phone"].ToString();
+                    CompanyFooterMassage = rd4["Footermsg"].ToString();
+                    CompanyLogo = rd4["CompanyLogo"].ToString();
+                }
+                cn.Close();
+            }
         }
+        catch { }
+        string imagePath = new Uri(Server.MapPath(CompanyLogo)).AbsoluteUri;
+
+        var reportParameters = new ReportParameterCollection
+               {
+                   new ReportParameter("CompanyName",CompanyName),
+                   new ReportParameter("ComapanyAddress",ComapanyAddress),
+                   new ReportParameter("CompanyMobileNumber",CompanyMobileNumber),
+                   new ReportParameter("CompanyWebsite",CompanyWebsite),
+                   new ReportParameter("CompanyFooterMassage",CompanyFooterMassage),
+                   new ReportParameter("CompanyLogo",imagePath)
+
+               };
 
 
+        ReportViewer1.ProcessingMode = ProcessingMode.Local;
+        ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/RDLCReports/DueInvoiceReport.rdlc");
+
+        ReportDataSource datasource = new ReportDataSource("DueInvoice", dueList);
+        ReportViewer1.LocalReport.DataSources.Clear();
+
+        ReportViewer1.LocalReport.EnableExternalImages = true;
+        ReportViewer1.ExportContentDisposition = ContentDisposition.AlwaysInline;
+
+        ReportViewer1.LocalReport.DataSources.Add(datasource);
+        ReportViewer1.LocalReport.SetParameters(reportParameters);
 
     }
+
 }
+
+
+
+
